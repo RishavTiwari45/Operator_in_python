5d606d3c3bc6ef50f65c1ec4028b70047e9e7f55
RishavTiwari45 <122211719+RishavTiwari45@users.noreply.github.com>
2023-01-08 18:28:11 +0530

    Add files via upload

---
+++ op.py
@@ -0,0 +1,249 @@
+#Opratare
+#Add numbers with input
+#a=int(input("enter the name"))
+#b=int(input("enter the name"))
+#c=int(input("enter the name"))
+#d=int(input("enter the name"))
+#e=int(input("enter the name"))
+#f=a+b+c+d+e
+#print(f)
+
+
+##radious
+#a=float(input("enter the name"))
+#b=int(input("enter the name"))
+#c=int(input("enter the name"))
+#d=a*b*c
+#print(d)
+
+
+
+#Radius=int(input("enter the name"))
+#pai=22/7
+#formula=pai*Radius**2
+#print("area of circle  :- ",formula)
+
+
+
+#a="rishav"
+#print(a[3])
+#print(len(a))
+
+#a="50"
+#b="40"
+#c=(int(a)+int(b))
+#print(c)
+
+#a=input("enter the name")*100
+#print(a)
+
+
+#a="1.5"
+#print(int(float(a)))
+
+#my_Var=45
+#myVar=10
+#print(my_Var+myVar)
+
+#a=frozenset(["apple","banana","mango"])
+#print(type(a))
+
+#a="abcdefgh"
+#print(a[7])
+
+#a=4
+#b=2
+#print(a!=b)
+
+#a=100
+#b=50
+#print(a or b) 
+#print(a and b)
+
+#output=11without+#int same na ho
+#a=6
+#b=5
+#print(int((a*2-b*2)/(a-b)))
+
+#a=8
+#b=7
+#print(int((a*2-b*2)/(a-b)))
+
+
+#percentage and average
+#a=int(input("math number:-"))
+#b=int(input("hindi number:-"))
+#c=int(input("english number:-"))
+#d=int(input("science number:-"))
+#e=int(input("chemistry number:-"))
+#f=int(input("total number:-"))
+#g=int(a+b+c+d+e)/f*100
+#h=int(a+b+c+d+e)/5
+#print(int(g))
+#print(int(h))
+
+
+#without int use integer me badalo
+#a="5"
+#b=eval(a)
+#print(type(b))
+
+
+#without use + sum two numbers
+#a=6
+#b=6
+#print(-(-a-b))
+
+#a=100
+#b=50
+#print(a and b)
+
+
+#area of trangle
+#a=int(input("base of trangle"))
+#b=(input("hight of trangle"))
+#c=(1/2*a*b)
+#print(c)
+
+
+#a=2+-+--5
+#print(a)
+
+#a,*b,c=1,2,3,4,5
+#print(a)
+#print(b)
+#print(c)
+
+#b,*a=1,2,3
+#print(a)
+
+#a=5+2j
+#b=abs(a)
+#print(type(b))
+
+#a=[1,2,3,4,5,6]
+#b=[1 in a]
+#print(b)
+
+#a=0 or 50
+#print(a)
+
+#a=("1")
+#print(bool(a))
+
+ 
+#print(0 and true )
+
+#a=100
+#b=5
+#print(a//b*a/b+a+b)
+
+#a=5
+#print(a  is int(a))
+
+#a="navgurukul"
+#print(a[ ::-1])
+
+
+#show program finish without anything printing
+#a="rishav"
+#b=""
+#a,b=b,a
+#print(a)
+
+#import keyword
+#print(keyword.kwlist)
+#
+#squarroot
+#a=int(input("enter the value"))
+#b=(a**(1/2))
+#print(b)
+
+#a=int(input("enter the value"))
+#b=a**0.5
+#print(b)
+
+
+#arthamatic oparetor in one variable
+#a=5
+#b=6
+#print(a+b-b*a/b**a%b//a)
+
+#find the perimeter and area of rectangle
+#a=int(input("enter the leanght:-"))
+#b=int(input("enter the breadth:-"))
+#perimeter=(a+b)*2
+#area=(a*b)
+#print(perimeter)
+#print(area)
+
+#diameter,circumference and area of circle
+#a=int(input("value of radius"))
+#diameter=2*(a)
+#print(diameter)
+#circumference=2*22/7*(a)
+#print(circumference)
+#area=(22/7*a**2)
+#print(area)
+
+#centimeter to meter,kilometer
+#a=int(input("value in centimeter:"))
+#b=a/100
+#c=a/100000
+#print(b)
+#print(c)
+
+#without float function value in float
+#a=6
+#b=3
+#print((a//b)*b/b)
+
+
+#without float function value in float
+#a=6
+#b=3
+#print(a//b*a/b)
+
+#error dega
+#a=6
+#b=3
+#c=4
+#print(a)
+#print(b)
+#print(c)
+#d=d
+#d=a
+
+##days to month,year
+#days=int(input("number of days:"))
+#week=days/7
+#month=days/12
+#year=days/365
+#print(month)
+#print(year)
+#print(week)
+
+
+#find the angle
+#a=int(input("angle A :"))
+#b=int(input("angle B :"))
+#c=(180-(a+b))
+#print(c)
+
+#area of equilaterel(sambahu) trangle
+#a=int(input("hight of trangle:"))
+#b=int(input("base of trangle:"))
+#c=(3**(1/3)/4*a*b)
+#print(c)
+
+
+#simple intrest
+#P=int(input("enter the value of P:"))
+#R=int(input("enter the value of R:"))
+#T=int(input("enter the time:"))
+#SI=(P*R*T)/100
+#print(SI)
+
+##compound intrest : error
+#P=int(input("enter the value of P :"))
+#R=int(input("enter the rate R: "))
